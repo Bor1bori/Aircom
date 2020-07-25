9daaf44625dbc98dccdf64694ddb164fff502276
김지현 <ledi000@naver.com>
2020-07-25 21:12:24 +0900

    Modify form of SignIn and SignUP so that it can match with API for authentication

--- frontend_web/pages/signIn.js
+++ frontend_web/pages/signIn.js
@@ -1,21 +1,40 @@
+import React from "react";
 import GoogleButton from "./googleButton";
 
-export default function SignIn() {
-  const handleClick = () => {};
-
-
-  return (
-    <div class="container auth">
-      <div class="input-field col s12">
+class SignIn extends React.Component {
+  state = {
+    email: "",
+    pw: "",
+  };
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+  handleClick = () =>{
+    console.log("login");
+  };
+  render() {
+    return (
+      <form>
         <label>이메일</label>
-        <input name="email" type="text" class="validate" />
-      </div>
-      <div class="input-field col s12">
+        <input
+          value={this.state.email}
+          onChange={this.handleChange}
+          name="email"
+        />
         <label>비밀번호</label>
-        <input name="password" type="password" class="validate" />
-      </div>
-      <button onClick={handleClick}>로그인</button>
-      <GoogleButton />
-    </div>
-  );
+        <input
+          type="password"
+          value={this.state.pw}
+          onChange={this.handleChange}
+          name="email"
+        />
+          <button type="submit" onClick={this.handleClick}>로그인</button>
+          <GoogleButton />
+      </form>
+    );
+  }
 }
+
+export default SignIn;
--- frontend_web/pages/signUp.js
+++ frontend_web/pages/signUp.js
@@ -1,21 +1,56 @@
-export default function SignUp() {
-    const handleClick = () => {};
+import React from "react";
+import Router from 'next/router';
 
-  return (
-    <div class="container auth">
-      <div class="input-field col s12">
-        <label>이름</label>
-        <input name="name" type="text" class="validate" />
-      </div>
-      <div class="input-field col s12 email">
+class SignUp extends React.Component {
+  state = {
+    email: "",
+    pw: "",
+    gender: "",
+    birthDate: "",
+  };
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+  handleClick = () => {
+    console.log("registered");
+    Router.push("/");
+  };
+  render() {
+    return (
+      <form>
         <label>이메일</label>
-        <input name="email" type="text" class="validate" />
-      </div>
-      <div class="input-field col s12">
+        <input
+          value={this.state.email}
+          onChange={this.handleChange}
+          name="email"
+        />
         <label>비밀번호</label>
-        <input name="password" type="password" class="validate" />
-      </div>
-      <button onClick={handleClick}>회원가입</button>
-    </div>
-  );
+        <input
+          type='password'
+          value={this.state.pw}
+          onChange={this.handleChange}
+          name="email"
+        />
+        <label>성별</label>
+        <select name="gender">
+          <option value="male">남성</option>
+          <option value="female">여성</option>
+          <option value="etc">기타</option>
+        </select>
+        <label>생년월일</label>
+        <input
+          value={this.state.birthDate}
+          onChange={this.handleChange}
+          name="birthDate"
+          text="생년월일 8자리" 
+        />
+        <button type="submit" onClick={this.handleClick}>
+          회원가입
+        </button>
+      </form>
+    );
+  }
 }
+export default SignUp;
