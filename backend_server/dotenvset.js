a48204baeb76ae8d7a150b83985ec6f054f6ff16
Kyutae Kim <rjaqor01@gmail.com>
2020-07-25 21:11:11 +0900

    Change dotenvset.ts to .js

---
+++ backend_server/dotenvset.js
@@ -0,0 +1,21 @@
+const dotenv = require('dotenv');
+const path = require('path');
+
+let configResult;
+switch (process.env.NODE_ENV) {
+  case 'prod':
+    configResult = dotenv.config({ path: path.join(__dirname, '.env.prod') });
+    break;
+  case 'develop':
+    configResult = dotenv.config({ path: path.join(__dirname, '.env.develop') });
+    break
+  case 'test':
+    configResult = dotenv.config({ path: path.join(__dirname, '.env.test') });
+    break
+  default:
+    throw new Error('process.env.NODE_ENV not set')
+    break;    
+}
+if (configResult.error) {
+  throw new Error(`.env.${process.env.NODE_ENV} not exists`)
+}
