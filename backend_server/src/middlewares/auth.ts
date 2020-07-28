7d9947e24488ac31585b06ed797418d3c3114cac
장우혁 <bor1boridv@gmail.com>
2020-07-28 12:37:44 +0900

    Update yup usage
    기존에서는 참조하는 Interface에 없는 property에 대한 스키마 조건이 들어가있어도 괜찮았지만
    이렇게 수정한 후에는 그럴 경우에 컴파일 에러를 일으킴
    ex) interface { id: string; } 에 대해서
    기존에는 { id: yup.string().required(), password: yup.string() } 이라 해도 괜찮았지만
    변경 후에는 password에 대한 내용이 interface에 없기 때문에 컴파일 에러를 일으키고
    { id: yup.string().required()와 같이 작성된 것만 허용됨

--- backend_server/src/middlewares/auth.ts
+++ backend_server/src/middlewares/auth.ts
@@ -1,11 +1,10 @@
 import { wrapper } from '@src/utils/wrapper';
 import * as yup from 'yup';
 import { SignupBody, SigninBody } from '@src/interfaces/auth';
-import { signup } from '@src/services/auth';
 
 export const signupValidator = wrapper(async (req, res, next) => {
 
-  const signupSchema: yup.ObjectSchema<SignupBody> = yup.object({
+  const signupSchema = yup.object<SignupBody>({
     email: yup.string().email().required(),
     password: yup.string().required(),
     birthdate: yup.date().max(new Date()).required(),
@@ -23,7 +22,7 @@ export const signupValidator = wrapper(async (req, res, next) => {
 
 export const signinValidator = wrapper(async (req, res, next) => {
   
-  const signinSchema: yup.ObjectSchema<SigninBody> = yup.object({
+  const signinSchema = yup.object<SigninBody>({
     email: yup.string().email().required(),
     password: yup.string().required(),
   }).required();
