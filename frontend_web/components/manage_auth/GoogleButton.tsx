b94d468afe9c11c2595058a9ce22a1da0049c5d0
장우혁 <bor1boridv@gmail.com>
2020-08-04 15:37:52 +0900

    Fix bug
    host 주소 잘못 되어있던거 고침

--- frontend_web/components/manage_auth/GoogleButton.tsx
+++ frontend_web/components/manage_auth/GoogleButton.tsx
@@ -12,7 +12,7 @@ const GoogleButton = () => {
     // Google Login
     const onResponseGoogle = (res: any) => {
         console.log(res);
-        axios.post("http://myaircom.co.kr:3000/auth/oauth/google/signin", {
+        axios.post(`${process.env.NEXT_PUBLIC_API_HOST}/auth/oauth/google/signin`, {
             idToken: res.wc.id_token
         })
             .then((res) => {
