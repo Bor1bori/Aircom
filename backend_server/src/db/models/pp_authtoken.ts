8f651dfbdf9eaf8543dea5cc2c7780d34ab6962d
장우혁 <bor1boridv@gmail.com>
2020-08-11 14:40:26 +0900

    [SE] Add PPAuthToken model for pp authorization
    원래는 local에서 오브젝트로 매핑하는 부분을 저장했는데 DB에 저장하는 걸로 변경

---
+++ backend_server/src/db/models/pp_authtoken.ts
@@ -0,0 +1,45 @@
+import {
+  Sequelize,
+  Model,
+  DataTypes
+} from 'sequelize';
+import { PCProvider } from './pc_provider';
+
+/* user db first settings */
+export interface PPAuthTokenAttributes {
+  authToken: string;
+  pcProviderId: number;
+}
+
+interface PPAuthTokeCreationAttributes extends PPAuthTokenAttributes {}
+
+export class PPAuthToken extends Model<PPAuthTokenAttributes, PPAuthTokeCreationAttributes>
+  implements PPAuthTokeCreationAttributes {
+  public authToken!: string;
+  public pcProviderId!: number;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+export const initPPAuthToken = (sequelize: Sequelize) => {
+  PPAuthToken.init({
+    authToken: {
+      type: DataTypes.STRING,
+      primaryKey: true
+    },
+    pcProviderId: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false
+    }
+  }, {
+    tableName: 'pp_authtoken',
+    sequelize
+  });
+};
+
+export const initPPAuthTokenAssociate = () => {
+  PPAuthToken.belongsTo(PCProvider, {
+    foreignKey: 'pcProviderId'
+  });
+};
