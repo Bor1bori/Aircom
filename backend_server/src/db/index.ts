23ffd527cdf541f1e05adb029dc3f23b2db45f53
장우혁 <bor1boridv@gmail.com>
2020-08-15 18:54:52 +0900

    [SE] Add pc_allocation model
    user과 pc 사이 일어난 할당 관계를 저장할 테이블
    추후 수정 예정

--- backend_server/src/db/index.ts
+++ backend_server/src/db/index.ts
@@ -3,6 +3,7 @@ import { initUser } from './models/user';
 import { initPCProvider, initPCProviderAssociate } from './models/pc_provider';
 import { initPC, initPCAssociate } from './models/pc';
 import { initPPAuthToken, initPPAuthTokenAssociate } from './models/pp_authtoken';
+import { initPCAllocation, initPCAllocationAssociate } from './models/pc_allocation';
 import createDebug from 'debug';
 
 const debug = createDebug('app');
@@ -30,10 +31,12 @@ export const sequelizeInit = async () => {
     initPCProvider(sequelize);
     initPC(sequelize);
     initPPAuthToken(sequelize);
+    initPCAllocation(sequelize);
 
     initPCAssociate();
     initPCProviderAssociate();
     initPPAuthTokenAssociate();
+    initPCAllocationAssociate();
     await sequelize.sync();
 
     // TODO: PPAUthToken 유효기간 지난 것들 삭제하기.
