0a22035f6aa9d170d7fa5a275a263b437d82e109
김지현 <ledi000@naver.com>
2020-08-15 20:28:42 +0900

    [AC] Capitalize PREF_LOGIN_TOKEN

--- client_app/app/src/main/java/com/aircom/data/SharedPreference.java
+++ client_app/app/src/main/java/com/aircom/data/SharedPreference.java
@@ -6,7 +6,7 @@ import android.preference.PreferenceManager;
 
 public class SharedPreference {
     static final String PREF_USER_NAME= "username";
-    static final String PREF_Login_Token = "loginToken";
+    static final String PREF_LOGIN_TOKEN = "loginToken";
 
     static SharedPreferences getSharedPreferences(Context ctx) {
         return PreferenceManager.getDefaultSharedPreferences(ctx);
@@ -15,13 +15,13 @@ public class SharedPreference {
     public static void setLoginToken(Context ctx, String loginToken)
     {
         SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
-        editor.putString(PREF_Login_Token, loginToken);
+        editor.putString(PREF_LOGIN_TOKEN, loginToken);
         editor.commit();
     }
 
     public static String getLoginToken(Context ctx)
     {
-        return getSharedPreferences(ctx).getString(PREF_Login_Token, "");
+        return getSharedPreferences(ctx).getString(PREF_LOGIN_TOKEN, "");
     }
 
     public static void setUserName(Context ctx, String userName)
