ec1dbf4a2723821c14bc9b69daffe6994d1ff9fd
김지현 <ledi000@naver.com>
2020-08-21 15:51:09 +0900

    [AC] Delete frontend_web for resolving merge conflicts

--- frontend_web/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { wrapper } from "../store/store";
-
-const WrappedApp = ({ Component, pageProps }: {Component: any; pageProps: any}) => {
-    return <Component {...pageProps} />;
-};
-
-export default wrapper.withRedux(WrappedApp);
