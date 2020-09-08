949057250e26e16b0178db1d6a6900ee09739a87
장우혁 <bor1boridv@gmail.com>
2020-09-08 22:46:41 +0900

    AIR-59[SE] Fix to catch error in PC Service

--- backend_server/src/services/pc.ts
+++ backend_server/src/services/pc.ts
@@ -74,8 +74,12 @@ export async function deallocatePCWithUser (user: User) {
     return -1;
   }
   const pc = await pcAllocation.getPC();
-  const terminateResult = await assureTermination(io, pc.uuid);
-  if (!terminateResult.success) {
+  try {
+    const terminateResult = await assureTermination(io, pc.uuid);
+    if (!terminateResult.success) {
+      return -2; // TODO 발생하면 안됨. 로그 기록, 처리 필요
+    }
+  } catch (err) {
     return -2; // TODO 발생하면 안됨. 로그 기록, 처리 필요
   }
   pc.state = 'usable'; // TODO: unusable로 설정하고 상태 관리를 통해 usable로 변경하도록 하기
