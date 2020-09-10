8a6f82e6b7d3fad8846450a919133a4289eb6ade
장우혁 <bor1boridv@gmail.com>
2020-09-10 18:55:51 +0900

    AIR-100[HO] Add comments in sio.py

--- host/sio.py
+++ host/sio.py
@@ -9,14 +9,16 @@ def signinCallback(data):
     print('signin 결과: ', data)
 
 @sio.event
-def connect():
+def connect(): # connect 될 경우 호출됨
     print('Connected!')
+    # TODO: uuid 실제 값을 쓰기.
     sio.emit('signin', {
         'uuid': '0fa822bf-d254-4cc3-9bbe-d26cab243c99'
     }, callback=signinCallback)
 
 @sio.on('allocate')
-def on_allocate(data):
+def on_allocate(data): # 반대쪽(Server)에서 allocate event를 emit할 경우 이 함수로 전달됨.
+    # TODO: UDP 홀펀칭 통해 ip, ports 받아오기.
     return {
         'success': True,
         'ip': '192.168.0.1',
@@ -25,12 +27,15 @@ def on_allocate(data):
 
 @sio.on('ask_state')
 def on_ask_state(data):
+    # TODO: p2p 연결 상태 확인해서 p2p_state에 저장
+    p2p_state = 'inUse'
     return {
-        'state': 'inUse'
+        'state': p2p_state
     }
 
 @sio.on('assure_termination')
 def on_assure_termination(data):
+    # TODO: P2P 연결이 되어있는 상태면 연결 종료시키기
     return {
         'success': True
     }
