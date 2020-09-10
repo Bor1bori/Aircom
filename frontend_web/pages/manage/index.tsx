331def1e7c1ada9f0356aa759e5b7aeb66c660c4
김지현 <ledi000@naver.com>
2020-09-11 01:12:23 +0900

    [WF] Delete unnecessary files
    폴더 구조 수정 및 파일 이동으로 남게 된 파일들.

--- frontend_web/pages/manage/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ManageLayout from "../../components/manage/layout/ManageLayout";
-
-export default function Index() {
-    return (
-        <ManageLayout/>
-    );
-}
-  
--- frontend_web/pages/signup.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import SignupForm from "../components/auth/SignupForm";
-
-export default function Signup() {
-    return (<SignupForm/>
-    );
-}
-  
