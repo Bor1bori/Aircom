664bd58a240091b98b66ad6e1f031d80960cff47
김지현 <ledi000@naver.com>
2020-09-11 01:00:08 +0900

    [WF] Update header

--- frontend_web/components/header/Header.tsx
+++ frontend_web/components/header/Header.tsx
@@ -1,37 +1,81 @@
 import React from "react";
 import Link from "next/link";
 import { useSelector } from "react-redux";
-import GoogleButton from "../auth/GoogleButton";
 import { RootState } from "../../store/store";
-
-const linkStyle = {
-    marginRight: 15
-};
 
 const Header = () => {
     const authState = useSelector((state: RootState) => state.auth);
+    const ppAuthState = useSelector((state: RootState) => state.ppAuth);
+    const state = authState.isSignedin ? "user" : ppAuthState.isSignedin ? "pp" : "none";
     return (
-        <div>
+        <div className="container">
             <Link href="/">
-                <a style={linkStyle}>Home</a>
+                <img src={require("../../styles/images/logo.png")} />
             </Link>
-            {authState.isSignedin ? <>
-                <a>로그아웃</a>
-            </>
-                : <>
-                    <Link href="/signin">
-                        <a style={linkStyle}>로그인</a>
+            {state == "none" ?
+                <Link href="/signinusertype">
+                    <a>로그인</a>
+                </Link> :
+                state == "user" ?
+                    <Link href="/user/accountinfo">
+                        <a>마이 페이지</a>
                     </Link>
-                    <Link href="/signup">
-                        <a style={linkStyle}>회원가입</a>
+                    :
+                    <Link href="/manager/manual">
+                        <a>관리 페이지</a>
                     </Link>
-                    <Link href="/manage">
-                        <a style={linkStyle}>관리페이지로 이동</a>
-                    </Link>
-                </>
             }
-
-            <GoogleButton/>
+            <button>
+                다운로드
+            </button>
+            <style jsx>{`
+                    *{
+                        font-size: 18px;
+                        font-family: "Apple SD Gothic"
+                    }
+                    .container{
+                        width: 1440px;
+                        height: 80px;
+                        box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.05);
+                        background-color: #ffffff;
+                        display: flex;
+                    }
+                    a {
+                        font-weight: normal;
+                        font-stretch: normal;
+                        font-style: normal;
+                        line-height: normal;
+                        letter-spacing: -0.17px;
+                        color: #000000;
+                        text-decoration: none;
+                        margin-right: 57px;
+                        margin-top: 28px;
+                    }
+                    button {
+                        width: 120px;
+                        height: 40px;
+                        border-radius: 20px;
+                        border: solid 1px #0052cc;
+                        font-weight: normal;
+                        font-stretch: normal;
+                        font-style: normal;
+                        line-height: normal;
+                        letter-spacing: -0.17px;
+                        color: #0052cc;
+                        background-color: #ffffff;
+                        margin-right: 123px;
+                        margin-top: 20px;
+                    }
+                    img {
+                        width: 101.8px;
+                        height: 24px;
+                        margin-left: 123px;
+                        margin-top: 31px;
+                        margin-bottom: 31px;
+                        margin-right: 820px;
+                    }
+                 `}
+            </style>
         </div>
     );
 };
