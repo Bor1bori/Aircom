af98d210272ff7f41de343c8fbf0ac3d7d238e65
WooHyeok Jang <bor1boridv@gmail.com>
2020-09-24 18:43:47 +0900

    [HO] Modify main.py
    불필요한 부분 삭제, 등록된지 확인하는 로직 추가

--- host/main.py
+++ host/main.py
@@ -1,31 +1,31 @@
 import socket
 import pywinauto
 from pywinauto.application import Application
-from streaming_tester import StreamingTester
 from api_sender import APISender
 import sio
+from config import Config
 
 CONF_DIRECTORY = "conf.ini"
 BACKEND_URL = "http://api.myaircom.co.kr"
 
-if __name__== "__main__" :
-    sio.init(BACKEND_URL)
-    auth_token = input("홈페이지에서 발급받은 인증코드를 입력하세요 : ")
-    print(auth_token)
-    tester = StreamingTester()
-    is_test_success = tester.test_streaming()
-    host_ip = tester.get_ip_address()
+config = Config(CONF_DIRECTORY)
+sender = APISender(BACKEND_URL)
 
-    sender = APISender(BACKEND_URL)
+if __name__== "__main__" :
+    uuid = config.getValue('id', 'uuid')
+    if (not uuid):
+        auth_token = input("홈페이지에서 발급받은 인증코드를 입력하세요 : ")
+        uuid = sender.register(auth_token, "192.168.0.1", 8080)
+        if uuid:
+            print("등록 완료!")
+            config.setValue("id", "uuid", uuid)
+        else:
+            print("authorized failed")
+            exit()
+
+    sio.init(BACKEND_URL)
+
     #sender.register("1234", host_ip)
-    uuid = sender.register(auth_token, "192.168.0.1", 8080)
-    if uuid:
-        print("authorized clear")
-        userfile = open(CONF_DIRECTORY, "w")
-        userfile.write(uuid)
-        userfile.close()
-    else:
-        print("authorized failed")
     '''
     sender = APISender(BACKEND_URL)
     sender.shield_connect()
