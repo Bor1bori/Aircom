3b18377063f7074fcb257a6555579ed588cfc3b2
김지현 <ledi000@naver.com>
2020-10-06 02:37:39 +0900

    [WF] Add IE browser detection

--- frontend_web/pages/index.tsx
+++ frontend_web/pages/index.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Layout from "../components/layout/Layout";
 import LandingPageForm from "../components/landingPage/LandingPageForm";
+import { isIE } from 'react-device-detect';
+import NoSsr from "../components/NoSSR";
 
 export default function Index() {
     return (
         <Layout>
-            <LandingPageForm/>
+            {isIE &&
+                <div>현재 브라우저는 지원되지 않는 브라우저 입니다.<br />
+                Chrome, Firefox, Safari 혹은 MS Edge를 통해 접속해 주세요.</div>}
+            <NoSsr>
+                <LandingPageForm />
+            </NoSsr>
         </Layout>
     );
 }
