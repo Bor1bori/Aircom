f7475dfc7668cf2413cfdc3e8471a4a36a553cf3
장우혁 <bor1boridv@gmail.com>
2020-10-06 21:51:40 +0900

    [SE] Add pc provider services
    내 pc목록, pc 사용 기록 가져오기

--- backend_server/src/services/pp.ts
+++ backend_server/src/services/pp.ts
@@ -1,6 +1,9 @@
+import { Pc } from '@src/db/models/pc';
+import { UsePc } from '@src/db/models/use_pc';
 import { PcProvider } from '@src/db/models/pc_provider';
 import { UpdatePPBody } from '@src/interfaces/pp';
 import { hash } from '@src/utils/crypto';
+import { Op } from 'sequelize';
 
 export const updatePP = async (ppId: number, updateBody: UpdatePPBody) => {
   if (updateBody.password) {
@@ -11,4 +14,28 @@ export const updatePP = async (ppId: number, updateBody: UpdatePPBody) => {
     return -1;
   }
   return await pp.update(updateBody);
+};
+
+export const getPcs = async (ppId: number) => {
+  const pcs = await Pc.findAll({
+    where: {
+      pcProviderId: ppId
+    }
+  });
+  return pcs;
+};
+
+export const getUsePcs = async (ppId: number) => {
+  const pcs = await getPcs(ppId);
+  const uuids = pcs.map(pc => pc.uuid);
+
+  const usePcs = await UsePc.findAll({
+    where: {
+      pcUuid: {
+        [Op.in]: uuids
+      }
+    }
+  });
+
+  return usePcs;
 };
