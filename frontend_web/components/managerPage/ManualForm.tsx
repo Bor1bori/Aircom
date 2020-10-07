b1205cfed6e062348fea2e820682d22b75564544
김지현 <ledi000@naver.com>
2020-10-08 01:01:46 +0900

    [WF] Get and display ppAuthToken

--- frontend_web/components/managerPage/ManualForm.tsx
+++ frontend_web/components/managerPage/ManualForm.tsx
@@ -1,4 +1,34 @@
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
+import axios from "axios";
+
 const Manual = () => {
+    const loginToken = useSelector((state: RootState) => state.ppAuth.ppLoginToken);
+    const [ppAuthToken, setPpAuthToken] = useState("");
+    const copyLoginToken = (e: any) => {
+        e.preventDefault();
+        const token = document.createElement("textarea");
+        document.body.appendChild(token);
+        token.value = loginToken;
+        token.select();
+        document.execCommand("copy");
+        document.body.removeChild(token);
+        alert("토큰이 복사되었습니다");
+    }
+    const getPpAuthToken = () => {
+        axios.get(`${process.env.NEXT_PUBLIC_API_HOST}/pp-auth/auth-token`,
+            { headers: { ppLoginToken: loginToken } })
+            .then((res) => {
+                setPpAuthToken(res.data.authToken);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+    useEffect(() => {
+        getPpAuthToken()
+    }, [loginToken]);
     return (
         <div className="container">
             <h1>Aircom 시작하기</h1>
@@ -6,6 +36,9 @@ const Manual = () => {
                 <p>1. 아래 링크를 통해 aircom host 1.3.1.exe를 다운로드합니다. </p>
                 <p>2. 다운로드 받은 파일을 설치 후, aircom 런쳐를 실행합니다.</p>
                 <p>3. PC 연결 버튼을 눌러 사용자의 PC를 aircom과 연결해줍니다.</p>
+                <p>4. 토큰 값을 입력해줍니다. (토큰 값 복사하기: &nbsp;
+                    <a onClick={copyLoginToken}>{ppAuthToken})</a>
+                </p>
                 <button>Host 프로그램 다운로드</button>
                 <p className="videoHeader">동영상으로 쉽게 따라해보세요.</p>
                 <video controls width="500" height="300">
@@ -55,6 +88,10 @@ const Manual = () => {
             video {
                 margin-top: 40px;
                 margin-bottom: 100px;
+            }
+            a {
+                color: #0052cc;
+                cursor: pointer;
             }
             `}</style>
         </div>
