3800f06395d74def179b691f6114d3b32af3f631
김지현 <ledi000@naver.com>
2020-10-10 21:26:52 +0900

    [WF] Add tile and favicon

--- frontend_web/pages/_app.tsx
+++ frontend_web/pages/_app.tsx
@@ -24,9 +24,16 @@ const WrappedApp = ({ Component, pageProps }: { Component: any; pageProps: any }
     return (
         <div>
             <Head>
+                <title>Aircom</title>
                 <link rel="stylesheet"
                     type="text/css"
                     href="https://cdn.rawgit.com/moonspam/NanumSquare/master/nanumsquare.css" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+                <link rel="manifest" href="/favicon/site.webmanifest" />
+                <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+                <meta name="theme-color" content="#0052cc" />
             </Head>
             <Component {...pageProps} />
             <style jsx global>{`
