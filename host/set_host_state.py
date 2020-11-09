4e283b4928565a55719f15bc85d100aefad675a6
Kyutae Kim <rjaqor01@gmail.com>
2020-11-09 21:39:56 +0900

    [HO] Add function of getting display resolution

--- host/set_host_state.py
+++ host/set_host_state.py
@@ -2,3 +2,13 @@ import os, winreg
 
 class SetComputer:
     def set_display_resolution(self):
+        proc_arch = os.environ['PROCESSOR_ARCHITECTURE'].lower()
+        arch_key = winreg.KEY_WOW64_64KEY
+        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER,
+                             r"Control Panel\Desktop\WindowMetrics", 0,
+                             winreg.KEY_READ | arch_key)
+        try:
+            dn = winreg.QueryValueEx(key, 'AppliedDPI')
+            return (dn[0]//96*100)
+        finally:
+            key.Close()
