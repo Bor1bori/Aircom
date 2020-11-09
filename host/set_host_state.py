5a1929fbe92a95099bc616352cbe01f06000b2ae
Kyutae Kim <rjaqor01@gmail.com>
2020-11-09 21:45:04 +0900

    [HO] Modify set_host_state.py about problem not running after finally

--- host/set_host_state.py
+++ host/set_host_state.py
@@ -2,6 +2,7 @@ import os, winreg
 
 class SetComputer:
     def set_display_resolution(self):
+        # only in 64bit
         proc_arch = os.environ['PROCESSOR_ARCHITECTURE'].lower()
         arch_key = winreg.KEY_WOW64_64KEY
         key = winreg.OpenKey(winreg.HKEY_CURRENT_USER,
@@ -9,6 +10,7 @@ class SetComputer:
                              winreg.KEY_READ | arch_key)
         try:
             dn = winreg.QueryValueEx(key, 'AppliedDPI')
-            return (dn[0]//96*100)
+            self.resolution = dn[0]//96*100)
         finally:
             key.Close()
+        return self.resolution
